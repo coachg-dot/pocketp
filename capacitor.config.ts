4e2cf46c455edd9f148babb8002a8f6a86944be8
Joshua Guptel <coachg@guptelpa.com>
2026-05-18 13:51:00 -0400

    Add Capacitor iOS

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.guptelpa.pocketpitcher',
+  appName: 'Pocket Pitcher',
+  webDir: 'dist'
+};
+
+export default config;
