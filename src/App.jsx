b4353a294849e40327e9421ddfb68f7122111d35
coachg-dot <coachg@guptelpa.com>
2026-05-29 15:10:00 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { HashRouter as Router } from 'react-router-dom';
 
 export default function App() {
   return (
-    <div style={{
-      minHeight: '100vh',
-      background: '#071326',
-      color: 'white',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
-      React Import Test
-    </div>
+    <Router>
+      <div style={{
+        minHeight: '100vh',
+        background: '#071326',
+        color: 'white',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        Router Test
+      </div>
+    </Router>
   );
 }
